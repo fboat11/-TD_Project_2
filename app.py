3f598a5869767119b1e046868075a9ae6e28f208
fboat11 <93167332+fboat11@users.noreply.github.com>
2021-11-22 09:44:48 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,44 @@ Created on Sun Nov 14 18:27:09 2021
 
 @author: frimt
 """
+import copy
 
-import constants
+from constants import PLAYERS
+
+alt_players = copy.deepcopy(PLAYERS)
+alt_teams = [] 
+
+
+
+Panthers = []
+Bandits = []
+Warriors = []
+for player in alt_players:
+    player['height'], units = player['height'].split()
+    player['height'] = int(player['height'])
+    
+    if player['experience'] == 'YES':
+        player['experience'] = True
+    else:
+        player['experience'] = False
+        
+experience = []
+inexperience = []
+
+for player in alt_players:
+    if player['experience'] == 'YES':
+        experience.append(player)
+    else:
+        inexperience.append(player)
+    
+print(experience)
+print(inexperience)
+
+    
+
+        
+      
+
 
 
 
